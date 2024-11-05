93aaeea2f60d64b4ca0f29805a3a85a0aaa16dfe
kechenk <rezhakechenk67@gmail.com>
2024-11-06 04:38:18 +0700

    [DUMP]

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,12 @@
             {{ __('Electra') }}
           </x-nav-link>
         </div>
+
+        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+          <x-nav-link :href="route('olympic')" :active="request()->routeIs('olympic')">
+            {{ __('Olympic') }}
+          </x-nav-link>
+        </div>
       </div>
 
       <!-- Settings Dropdown -->
