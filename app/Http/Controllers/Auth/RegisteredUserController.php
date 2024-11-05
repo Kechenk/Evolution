fedc5983e0325461be6cee52708128a731987c9e
kechenk <rezhakechenk67@gmail.com>
2024-11-05 16:55:23 +0700

    [HOTFIX] - Route
    Revert All Change

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -51,7 +51,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        // return redirect(route('dashboard', absolute: false));
-        return redirect(route('dasbor', absolute: false));
+        return redirect(route('dashboard', absolute: false));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,9 @@ Route::get('/masuk', function () {
     return view('masuk');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/dasbor', function () {
-    return view('dasbor');
-})->middleware(['auth', 'verified'])->name('dasbor');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
