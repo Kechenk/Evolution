43497110ad0e9bf2dbe8b7ed18aa90a49c0a19a8
kechenk <rezhakechenk67@gmail.com>
2024-11-06 06:25:45 +0700

    [HOTFIX] -- Dashboard
    Mengganti class laravel menjadi html

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
       </div>
     </div>
   </div>
-<a href="{{ route('electra') }}">
+<a href="/electra">
   <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
     <div class="bg-green-400 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg">
       <div class="p-6 text-gray-900 dark:text-gray-100 text-2xl">
