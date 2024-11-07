c6a164687caf3a4aead9610917192da7bdb9fb5c
kechenk <rezhakechenk67@gmail.com>
2024-11-07 18:26:04 +0700

    [FIX] - Delete 1 Line

--- database/migrations/2024_11_05_185221_create_solo_participant_table.php
+++ database/migrations/2024_11_05_185221_create_solo_participant_table.php
@@ -4,7 +4,6 @@ use Faker\Guesser\Name;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
   /**
